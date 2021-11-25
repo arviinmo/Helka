5d39b31ed14e238f5dd51d179b604e53b6d86537
Arvin mostafaei <Arvinmostafaeii@gmail.com>
2021-11-25 12:45:41 +0330

    fixed the header bar

--- gatsby-config.js
+++ gatsby-config.js
@@ -169,7 +169,7 @@ module.exports = {
         short_name: `helka`,
         start_url: `/`,
         background_color: `#ffffff`,
-        theme_color: `#0F2C67`,
+        theme_color: `#ffa7c4`,
         display: `minimal-ui`,
         icon: `src/assets/icon.png`,
         theme_color_in_head: false,
