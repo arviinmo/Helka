7dba6b6157822b511dc2b486d3bff6c151d4766e
Arvîn mostafaei <Arvinmostafaeii@gmail.com>
2022-08-25 16:25:07 +0430

    add new picture!

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import profilePic from '../assets/pic.jfif';
+import profilePic from '../assets/pic.jpg';
 import { rhythm } from '../utils/typography';
 
 class Bio extends React.Component {
@@ -25,23 +25,25 @@ class Bio extends React.Component {
             margin: 'auto',
             justifycontent: 'center',
             display: 'flex',
-            flexDirection: 'column'
+            flexDirection: 'column',
           }}
         />
-        <p style={{ 
-          maxWidth: 410,
-          justifycontent: 'center',
-          alignItems: 'center',
-          textAlign: 'center',
-          margin: 'auto',
-          fontSize: rhythm(0.8),
-          marginTop: rhythm(1),
-          marginBottom: rhythm(2.5),
-          fontFamily: 'Montserrat, sans-serif'
-        }}>
+        <p
+          style={{
+            maxWidth: 410,
+            justifycontent: 'center',
+            alignItems: 'center',
+            textAlign: 'center',
+            margin: 'auto',
+            fontSize: rhythm(0.8),
+            marginTop: rhythm(1),
+            marginBottom: rhythm(2.5),
+            fontFamily: 'Montserrat, sans-serif',
+          }}
+        >
           Personal blog by{' '}
-          <a href="https://arvin.vercel.app">Arvin Mostafaei</a>.{' '}
-          I&nbsp;explain with words and code.
+          <a href="https://arvin.vercel.app">Arvin Mostafaei</a>. I&nbsp;explain
+          with words and code.
         </p>
       </div>
     );
